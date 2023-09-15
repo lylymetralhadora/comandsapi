91bc10a4014163b30709cec5cb4eac9ee5129955
lylymetralhadora <clc.cheshirebr@gmail.com>
2023-09-15 18:09:38 -0300

    api

--- src/server.js
+++ src/server.js
@@ -7,5 +7,21 @@ const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
 
+app.get('/api', (request, response) => {
+    response.send('retorno de informações do banco de dados');
+});
+
+app.post('/api/:id', (request, response) => {
+    response.send('metodo utilizado para salvar informações')
+});
+
+app.put('/api/:id', (request, response) => {
+    response.send('metodo utilizado para editar informações')
+});
+
+app.delete('/api/:id', (request, response) => {
+    response.send('metodo utilizado para deletar informações')
+});
+
 //testar servidor
 app.listen(PORT, () => console.log(`Running at port ${PORT}`))
